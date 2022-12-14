98c1138663088f57fc187d7fd5d76d922db1a0e5
Federico Valdiglesias <federicomrtl@gmail.com>
2022-12-14 16:04:01 -0300

    Elimine archivos innecesarios del frontend del footer

--- src/routers/RouterPrincipal.jsx
+++ src/routers/RouterPrincipal.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
-import Header from "../layout/Header/Header";
 import Footer from "../components/layout/Footer/Footer";
 
 export const RouterPrincipal = () => {
   return (
     <div>
       <BrowserRouter>
-        <Header />
         <Routes>
         </Routes>
         <Footer />
